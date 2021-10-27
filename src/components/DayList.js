902def31cd887134c8ebabbd9bcea3c168ea314d
Catherine Swain <catherinemexicoswain@gmail.com>
2021-10-27 09:40:59 -0400

    daylist as dynatic

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -1,8 +1,23 @@
 import React from "react";
 
 import "components/DayListItem";
+import DayListItem from "components/DayListItem";
 
 export default function DayList (props) {
+  const Days = props.days.map((day) => {
 
-  return <ul></ul>
+  return (
+    <DayListItem
+    key={day.id}
+    id={day.id}
+    name={day.name}
+    spots={day.spots}
+    selected={day.name === props.day}
+    setDay={props.setDay}
+    />
+  )
+    
+  })
+
+  return <ul>{Days}</ul>
 }
