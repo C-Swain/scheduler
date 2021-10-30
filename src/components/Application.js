1e504ac5343af12c961382b9983c122f22537c31
Catherine Swain <catherinemexicoswain@gmail.com>
2021-10-30 11:06:54 -0400

    Managing State

--- src/components/Application.js
+++ src/components/Application.js
@@ -21,48 +21,21 @@ const interviewers = [
   { id: 5, name: "Sven Jones", avatar: "https://i.imgur.com/twYrpay.jpg" }
 ];
 
-const appointments = [
-  {
-    id: 1,
-    time: "12pm",
-  },
-  {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer:{
-        id: 3,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  {
-    id: 3,
-    time: "2pm",
-  },
-  {
-    id: 4,
-    time: "3pm",
-    interview: {
-      student: "Archie Andrews",
-      interviewer:{
-        id: 4,
-        name: "Cohana Roy",
-        avatar: "https://i.imgur.com/FK8V841.jpg",
-      }
-    }
-  },
-  {
-    id: 5,
-    time: "4pm",
-  }
-];
 
 export default function Application(props) {
-  const [day, setDay] = useState("Monday");
-  const [days, setDays] = useState([]);
+  
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    
+    appointments: {}
+  });
+
+  const dailyAppointments = [];
+
+const setDay = day => setState(prev => ({ ...prev, day}));
+const setDays = days => setState(prev => ({ ...prev, days }));
+
 // .axios must live in a function
 useEffect(() => {
   const daysURL = `/api/days`
@@ -70,6 +43,7 @@ useEffect(() => {
     console.log(response.data);
     setDays([...response.data])
   });
+
   //empty array means it only fills once 
 }, []);
   const parsedAppointments = appointments.map((appointment) =>
@@ -84,7 +58,7 @@ useEffect(() => {
 />
 <hr className="sidebar__separator sidebar--centered" />
 <nav className="sidebar__menu">
-  <DayList days={days} value={day} onChange={setDay}/>
+  <DayList days={state.days} value={state.day} onChange={setDay}/>
   </nav>
   <InterviewerList interviewers={interviewers} setInterviewer={console.log} interviewer={interviewer.id}/>
 <img
--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,5 +1,27 @@
 
 
+
 export function getAppointmentsForDay(state, day) {
+
+ // First check what appoinments are on day, if none return empty array
+  let onDay = state.days.filter(d => d.name === day)[0];
+  if(!onDay) {
+   return [];
+  }
+
+  let result = [];
+
+//next we compare the appointment Id and push detials to the result 
+  for(const id of onDay.appointments){
+    const appointmentObj = state.appointments[id];
+    console.log(appointmentObj)
+    result.push(appointmentObj);
+  }
+  return result
+
+ }
+
+
  
-}
+
+ 
