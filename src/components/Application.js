1779ea3a3c7e485e6113d5ec5368a292cd858a32
Catherine Swain <catherinemexicoswain@gmail.com>
2021-10-30 16:03:11 -0400

    getInterviewer added

--- src/components/Application.js
+++ src/components/Application.js
@@ -4,24 +4,8 @@ import "components/Application.scss";
 import DayList from "./DayList";
 import InterviewerList from "./InterviewerList";
 import Appointment from "components/Appointment";
-import { getAppointmentsForDay } from "helpers/selectors";
+import { getAppointmentsForDay, getInterview } from "helpers/selectors";
 
-
-const interviewer = {
-  id: 1,
-  name: "Sylvia Palmer",
-  avatar: "https://i.imgur.com/LpaY82x.png"
-
-};
-
-//next step is remove this hardcoding for interviewers
-const interviewers = [
-  { id: 1, name: "Sylvia Palmer", avatar: "https://i.imgur.com/LpaY82x.png" },
-  { id: 2, name: "Tori Malcolm", avatar: "https://i.imgur.com/Nmx0Qxo.png" },
-  { id: 3, name: "Mildred Nazir", avatar: "https://i.imgur.com/T2WwVfS.png" },
-  { id: 4, name: "Cohana Roy", avatar: "https://i.imgur.com/FK8V841.jpg" },
-  { id: 5, name: "Sven Jones", avatar: "https://i.imgur.com/twYrpay.jpg" }
-];
 
 
 export default function Application(props) {
@@ -33,27 +17,36 @@ export default function Application(props) {
     interviewers: {}
   });
 
-const dailyAppointments = getAppointmentsForDay(state, state.day);
-
 const setDay = day => setState(prev => ({ ...prev, day}));
 useEffect(() => {
   let days = axios.get('/api/days');
   let appointments = axios.get('/api/appointments');
   let interviewers = axios.get('/api/interviewers');
-  let dailyAppointments = getAppointmentsForDay(state,state.day)
+  
 
 Promise.all([days, appointments, interviewers]).then(results => {
   days = results[0].data;
   appointments = results[1].data;
-  interviewers = results[2].data
-
+  interviewers = results[2].data;
+  
   setState(prev => ({ ...prev, days, appointments, interviewers }));
 });
 }, []);
 
+const appointments = getAppointmentsForDay(state, state.day);
 
-  const parsedAppointments = dailyAppointments.map(appointment => 
-   <Appointment key={appointment.id} {...appointment} />);
+const schedule = appointments.map((appointment) => {
+  const interview = getInterview(state, appointment.interview);
+
+  return (
+    <Appointment
+      key={appointment.id}
+      id={appointment.id}
+      time={appointment.time}
+      interview={interview}
+    />
+  );
+});
   return (
     <main className="layout">
       <section className="sidebar">
@@ -68,7 +61,7 @@ Promise.all([days, appointments, interviewers]).then(results => {
   <DayList days={state.days} value={state.day} onChange={setDay}/>
   </nav>
 
-  <InterviewerList interviewers={interviewers} setInterviewer={console.log} interviewer={interviewer.id}/>
+  
 <img
   className="sidebar__lhl sidebar--centered"
   src="images/lhl.png"
@@ -76,7 +69,8 @@ Promise.all([days, appointments, interviewers]).then(results => {
 />
       </section>
       <section className="schedule">
-    {parsedAppointments}
+    {schedule}
+    <Appointment key="last" time="5pm" />
    </section>
     </main>
   );
--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -3,10 +3,6 @@
 
 export function getAppointmentsForDay(state, day) {
 
-  //what to do if there are no appointments 
-  if (!state.days) {
-    return [];
-  }
   
    // First check what appoinments are on day, if none return empty array
    let onDay = state.days.filter(d => d.name === day)[0];
@@ -17,14 +13,29 @@ export function getAppointmentsForDay(state, day) {
    let result = [];
  
  //next we compare the appointment Id and push detials to the result 
-   for(const id of onDay.appointments){
+   for(const id of onDay.appointments) {
      const appointmentObj = state.appointments[id];
      console.log(appointmentObj)
      result.push(appointmentObj);
    }
    return result
  
+}
+
+export function getInterview(state, interview) {
+    //if no interview it returns null
+    if (!interview) {
+
+    return null;
+   }
+    //if there is interview there must be details
+    const interviewerID = interview.interviewer;
+    const interviewerObj = state.interviewers[interviewerID]
+    const result = {student: interview.student, interviewer: interviewerObj}
+    return result;
   }
+  
+
  
 
 
